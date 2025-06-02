4caeab871de9a7fb3948b3af79d4dba76ddf3481
acikgozb <acikgozb@proton.me>
2025-06-02 16:21:46 +0300

    feat(ls): Implement `bt list-devices`
    `bt list-devices` (or `bt ls` with alias) is implemented to show the known devices to the host.
    
    It is designed to work in a similar way to `nmcli`. Like `nmcli`, it can provide the output in two different ways:
    
    - A pretty output (table format),
    - A terse output (for scripting).
    
    Also, the output is designed to be filterable by specifying the field keys (e.g. "alias,connected), like `nmcli`.
    
    Signed-off-by: acikgozb <acikgozb@proton.me>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 pub mod api;
 
+use std::fmt::Debug;
 use std::{error, fmt, io};
+use tabled::builder::Builder;
+use tabled::settings::Style;
 use zbus::blocking::Connection;
 use zbus::blocking::fdo::ObjectManagerProxy;
 use zbus::proxy;
@@ -247,6 +250,42 @@ pub fn toggle(f: &mut impl io::Write) -> Result<(), Box<dyn error::Error>> {
 
     Ok(())
 }
+
+enum BtListingOutput {
+    Pretty,
+    Terse,
+}
+
+pub trait BtListingConverter {
+    fn get_field_with_key(&self, value: &BtListingKey) -> Box<&dyn fmt::Display>;
+    fn filter_by_status(&self, value: &Option<BtListingStatusKey>) -> bool;
+}
+
+impl BtListingConverter for BluezDev {
+    fn get_field_with_key(&self, value: &BtListingKey) -> Box<&dyn fmt::Display> {
+        match value {
+            BtListingKey::Alias => Box::new(&self.alias),
+            BtListingKey::Address => Box::new(&self.address),
+            BtListingKey::Connected => Box::new(&self.connected),
+            BtListingKey::Trusted => Box::new(&self.trusted),
+            BtListingKey::Bonded => Box::new(&self.bonded),
+            BtListingKey::Paired => Box::new(&self.paired),
+        }
+    }
+
+    fn filter_by_status(&self, value: &Option<BtListingStatusKey>) -> bool {
+        match value {
+            Some(key) => match key {
+                BtListingStatusKey::Connected => self.connected,
+                BtListingStatusKey::Trusted => self.trusted,
+                BtListingStatusKey::Bonded => self.bonded,
+                BtListingStatusKey::Paired => self.paired,
+            },
+            None => true,
+        }
+    }
+}
+
 // TODO: Move this under API crate.
 #[derive(Copy, Clone, Debug, clap::ValueEnum)]
 pub enum BtListingKey {
@@ -258,6 +297,21 @@ pub enum BtListingKey {
     Paired,
 }
 
+impl From<&BtListingKey> for String {
+    fn from(value: &BtListingKey) -> Self {
+        let str = match value {
+            BtListingKey::Alias => "ALIAS",
+            BtListingKey::Address => "ADDRESS",
+            BtListingKey::Connected => "CONNECTED",
+            BtListingKey::Trusted => "TRUSTED",
+            BtListingKey::Bonded => "BONDED",
+            BtListingKey::Paired => "PAIRED",
+        };
+
+        str.to_string()
+    }
+}
+
 #[derive(Debug, Copy, Clone, clap::ValueEnum)]
 pub enum BtListingStatusKey {
     Connected,
@@ -266,3 +320,83 @@ pub enum BtListingStatusKey {
     Paired,
 }
 
+const DEFAULT_LISTING_KEYS: [BtListingKey; 6] = [
+    BtListingKey::Alias,
+    BtListingKey::Address,
+    BtListingKey::Connected,
+    BtListingKey::Trusted,
+    BtListingKey::Bonded,
+    BtListingKey::Paired,
+];
+
+// TODO: Now the time comes to create a module this, along with other subcommands.
+pub fn list_devices(
+    f: &mut impl io::Write,
+    columns: Option<Vec<BtListingKey>>,
+    values: Option<Vec<BtListingKey>>,
+    status: Option<BtListingStatusKey>,
+) -> Result<(), Box<dyn error::Error>> {
+    let (out_format, user_listing_keys) = match (columns, values) {
+        (None, None) => (BtListingOutput::Pretty, None),
+        (None, values) => (BtListingOutput::Terse, values),
+        (columns, _) => (BtListingOutput::Pretty, columns),
+    };
+
+    let listing_keys = match user_listing_keys {
+        Some(keys) => keys,
+        None => DEFAULT_LISTING_KEYS.to_vec(),
+    };
+
+    let bluez = Bluez::new()?;
+    let devs = bluez.devs()?;
+
+    let listing = devs.iter().filter_map(|dev| {
+        if !dev.filter_by_status(&status) {
+            None
+        } else {
+            Some(
+                listing_keys
+                    .iter()
+                    .map(|k| dev.get_field_with_key(k).to_string())
+                    .collect::<Vec<String>>(),
+            )
+        }
+    });
+
+    let out_buf = match out_format {
+        BtListingOutput::Pretty => create_pretty_out(listing, &listing_keys),
+        BtListingOutput::Terse => create_terse_out(listing),
+    };
+
+    f.write_all(out_buf.as_bytes())?;
+
+    Ok(())
+}
+
+pub fn create_pretty_out(
+    listing: impl Iterator<Item = Vec<String>>,
+    columns: &[BtListingKey],
+) -> String {
+    let mut builder = Builder::default();
+
+    builder.push_record(columns);
+
+    for row in listing {
+        builder.push_record(row);
+    }
+
+    let mut table = builder.build();
+    table.with(Style::blank());
+
+    format!("{}", table)
+}
+
+pub fn create_terse_out(listing: impl Iterator<Item = Vec<String>>) -> String {
+    listing
+        .map(|l| {
+            let mut terse_str = l.join("/");
+            terse_str.push('\n');
+            terse_str
+        })
+        .collect()
+}
