7dae33f38ee9d10e475decffa3ce88b79147590f
acikgozb <acikgozb@proton.me>
2025-06-12 13:20:44 +0300

    refactor(api): Move ScanArgs under scan module
    Signed-off-by: acikgozb <acikgozb@proton.me>

--- src/scan.rs
+++ src/scan.rs
@@ -1,6 +1,28 @@
 use std::{error, io};
 
-use crate::api::ScanArgs;
+use clap::Args;
+
+#[derive(Debug, Args)]
+pub struct ScanArgs {
+    /// Set the duration of the scan.
+    #[arg(short, long, default_value_t = 5u8)]
+    pub duration: u8,
+
+    /// Filter the pretty output based on given columns.
+    #[arg(short, long, value_delimiter = ',')]
+    pub columns: Option<Vec<BtScanColumns>>,
+
+    /// Filter the terse output based on given columns.
+    #[arg(short, long, value_delimiter = ',')]
+    pub values: Option<Vec<BtScanColumns>>,
+}
+
+#[derive(Debug, Copy, Clone, clap::ValueEnum)]
+pub enum BtScanColumns {
+    Alias,
+    Address,
+    RSSI,
+}
 
 pub fn scan(f: &mut impl io::Write, args: &ScanArgs) -> Result<(), Box<dyn error::Error>> {
     todo!()
