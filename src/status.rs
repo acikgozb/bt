2aac6dcce6f7e6fa60afc5e8c9d46d41a4133961
acikgozb <acikgozb@proton.me>
2025-06-20 19:48:47 +0300

    feat(status): Add status errors
    Signed-off-by: acikgozb <acikgozb@proton.me>

--- src/status.rs
+++ src/status.rs
@@ -1,12 +1,45 @@
-use std::{error, io};
+use std::{error, fmt, io};
 
 use crate::bluez;
 
-pub fn status(f: &mut impl io::Write) -> Result<(), Box<dyn error::Error>> {
-    let bluez = bluez::Client::new()?;
+#[derive(Debug)]
+pub enum Error {
+    PowerState(bluez::Error),
+    ConnectedDevices(bluez::Error),
+    DBusClient(bluez::Error),
+    Io(io::Error),
+}
 
-    let power_state = bluez.power_state()?;
-    let connected_devs = bluez.connected_devs()?;
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Error::PowerState(error) => {
+                write!(f, "unable to get device power state: {}", error)
+            }
+            Error::ConnectedDevices(error) => {
+                write!(f, "unable to get connected devices: {}", error)
+            }
+            Error::DBusClient(error) => {
+                write!(f, "unable to establish a D-Bus connection: {}", error)
+            }
+            Error::Io(error) => write!(f, "io error: {}", error),
+        }
+    }
+}
+
+impl error::Error for Error {}
+
+impl From<io::Error> for Error {
+    fn from(value: io::Error) -> Self {
+        Self::Io(value)
+    }
+}
+
+pub fn status(f: &mut impl io::Write) -> Result<(), Error> {
+    let bluez = bluez::Client::new().map_err(Error::DBusClient)?;
+
+    let power_state = bluez.power_state().map_err(Error::PowerState)?;
+    let connected_devs = bluez.connected_devs().map_err(Error::ConnectedDevices)?;
 
     let mut buf = [
         "bluetooth: ",
