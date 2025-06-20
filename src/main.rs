4f5a32eae56e71860a57533ec0ae15bd1abaa6e2
acikgozb <acikgozb@proton.me>
2025-06-20 19:49:53 +0300

    feat(main): Print errors through stderr
    The entrypoint of `bt` is updated to print the errors retrieved from
    subcommands to stderr stream.
    
    Signed-off-by: acikgozb <acikgozb@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,43 @@ use std::{error, io, process::ExitCode};
 use bt::api::{BtCommand, Cli};
 use clap::Parser;
 
+const PROGRAM: &str = "bt";
+
 fn main() -> ExitCode {
     match run() {
         Ok(_) => ExitCode::SUCCESS,
-        Err(_) => ExitCode::FAILURE,
+        Err(e) => {
+            eprintln!("{PROGRAM}: {}", e);
+
+            ExitCode::FAILURE
+        }
     }
 }
 
 fn run() -> Result<(), Box<dyn error::Error>> {
     let args = Cli::parse();
 
-    println!("{:?}", args);
-
     let mut stdout = io::stdout();
     let stdin = io::stdin();
 
     if let Some(subcommand) = args.command {
         match subcommand {
-            BtCommand::Status => bt::status(&mut stdout),
-            BtCommand::Toggle => bt::toggle(&mut stdout),
-            BtCommand::Scan { args } => bt::scan(&mut stdout, &args),
+            BtCommand::Status => bt::status(&mut stdout)?,
+            BtCommand::Toggle => bt::toggle(&mut stdout)?,
+            BtCommand::Scan { args } => bt::scan(&mut stdout, &args)?,
             BtCommand::Connect { args } => {
                 let mut locked_stdin = stdin.lock();
-                bt::connect(&mut stdout, &mut locked_stdin, &args)
+                bt::connect(&mut stdout, &mut locked_stdin, &args)?
             }
             BtCommand::Disconnect { force, aliases } => {
                 let mut locked_stdin = stdin.lock();
-                bt::disconnect(&mut stdout, &mut locked_stdin, &force, &aliases)
+                bt::disconnect(&mut stdout, &mut locked_stdin, &force, &aliases)?
             }
-            BtCommand::ListDevices { args } => bt::list_devices(&mut stdout, &args),
+            BtCommand::ListDevices { args } => bt::list_devices(&mut stdout, &args)?,
         }
     } else {
-        bt::status(&mut stdout)
-    }?;
+        bt::status(&mut stdout)?
+    };
 
     Ok(())
 }
